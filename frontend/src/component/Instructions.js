da4452e6ce7165b45088a9a0884ddc20cc73961c
Vyshnavi Kurella <vyshnavikurella456@gmail.com>
2023-08-05 08:06:28 -0700

    added content for intro and instructions to play

--- frontend/src/component/Instructions.js
+++ frontend/src/component/Instructions.js
@@ -12,7 +12,17 @@ const Instructions = () => {
             <Grid container  direction="row"  justifyContent="center" alignContent="center" alignItems="center">
               <Grid item xs={10} >
             <Typography className="homePage_typography" variant="h4" color="textSecondary" component="div">
-              How to play instructions to be displayed here
+            Instructions to Play Trickster Roads:
+            <Typography className="homePage_typography" variant="h6" color="textSecondary" component="div">
+Welcome to Trickster Roads, where you'll take on the role of a driver in an exciting Autonomous Vehicle (AV) simulation. Your goal is to navigate through the map and accomplish two key objectives:
+<div> 1. Earn "Pengci" Score:</div>
+    Test your skills in outsmarting the other AVs on the road. Trick them into behaving incorrectly by creating challenging traffic events such as causing collisions, violating traffic signals, or executing unexpected maneuvers. The more successfully you trick the AVs, the higher your "Pengci" score will be.
+<div> 2. Earn "Event" Score:</div>
+    Be on the lookout for other autonomous vehicles attempting to deceive you. Avoid falling into their traps, as they will try to lead you into making mistakes, such as getting involved in collisions or violating traffic rules. Your ability to stay alert and evade these tricky AVs will increase your "Event" score.
+<div>
+As you immerse yourself in this thrilling experience, keep a close eye on your scores and strive to become a master of AV simulation. Remember, a delicate balance between tricking the AVs and avoiding their tricks will lead you to triumph on Trickster Roads. Are you ready to showcase your strategic prowess and driving skills in this adrenaline-pumping adventure? Get behind the virtual wheel and embark on the journey to prove yourself as the ultimate Trickster Roads champion! Good luck, and may the roads be ever in your favor!
+ </div>
+            </Typography>
             </Typography>
             </Grid>
          
--- frontend/src/component/Pengci.js
+++ frontend/src/component/Pengci.js
@@ -119,7 +119,10 @@ const Pengci = () => {
         <Grid container  direction="row" spacing={2}   >
           <Grid item xs={5} >
         <Typography className="homePage_typography" variant="h4" color="textSecondary" component="div">
-          Pengci
+          Trickster Roads
+        </Typography>
+        <Typography className="homePage_typography" variant="h6" color="textSecondary" component="div">
+        The ultimate game that puts you in the driver's seat of an Autonomous Vehicle (AV) simulation. In this cutting-edge experience, you will delve into the world of AVs and learn how simulation can be used to test their robustness in handling challenging traffic scenarios. Strap yourself in for an adrenaline-pumping adventure where you'll encounter various traffic events designed to push your AV's capabilities to the limit. Can you outwit other AVs that attempt to trick you into making mistakes? By mastering these events, you'll unlock the secrets to navigate through tricky situations and gain the skills to potentially outsmart your rivals. So, gear up for the ride of a lifetime on Trickster Roads, where you'll embark on an exhilarating journey of discovery and strategy in the realm of Autonomous Vehicles!
         </Typography>
         <Typography className="homePage_typography" variant="h4" color="textSecondary" component="div">
           Pengci Game info
